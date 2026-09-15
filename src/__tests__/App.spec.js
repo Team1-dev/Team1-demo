de39ccc62eace6b08a99a62cbaf6919479c8740b
Team1 <hello@tryteam1.com>
2026-09-15 13:38:08 +0100

    Add todos (#8)
    Closes #2
    
    Co-authored-by: Team1-dev <315909628+Team1-dev@users.noreply.github.com>

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -4,8 +4,32 @@ import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('adds a todo item to the list when submitted', async () => {
     const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+
+    await wrapper.find('input').setValue('Buy milk')
+    await wrapper.find('form').trigger('submit')
+
+    const items = wrapper.findAll('li')
+    expect(items).toHaveLength(1)
+    expect(items[0].text()).toBe('Buy milk')
+  })
+
+  it('clears the input after adding a todo', async () => {
+    const wrapper = mount(App)
+
+    await wrapper.find('input').setValue('Buy milk')
+    await wrapper.find('form').trigger('submit')
+
+    expect(wrapper.find('input').element.value).toBe('')
+  })
+
+  it('does not add empty todos', async () => {
+    const wrapper = mount(App)
+
+    await wrapper.find('input').setValue('   ')
+    await wrapper.find('form').trigger('submit')
+
+    expect(wrapper.findAll('li')).toHaveLength(0)
   })
 })
